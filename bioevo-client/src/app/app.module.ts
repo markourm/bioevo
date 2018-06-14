8546983856e0d75eed23bbb1100aaf26e4363476
Marko Urm <markourm@gmail.com>
2018-06-14 12:07:33 +0100

    removed unneeded HttpClient import

--- bioevo-client/src/app/app.module.ts
+++ bioevo-client/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
@@ -13,7 +12,6 @@ import { BioevoFrontComponent } from './bioevo-front/bioevo-front.component';
   ],
   imports: [
     BrowserModule,
-    HttpModule,
     HttpClientModule
   ],
   providers: [],
--- bioevo-client/src/app/shared/report/report.service.ts
+++ bioevo-client/src/app/shared/report/report.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { HttpClientModule } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
