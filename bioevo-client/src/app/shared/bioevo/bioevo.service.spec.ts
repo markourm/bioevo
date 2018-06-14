0c57b4cc7456bbfdc6e6dce38f476962ef52286f
Marko Urm <markourm@gmail.com>
2018-06-14 14:21:44 +0100

    updated service unit tests to use HttpClientTestingModule

--- bioevo-client/src/app/shared/bioevo/bioevo.service.spec.ts
+++ bioevo-client/src/app/shared/bioevo/bioevo.service.spec.ts
@@ -1,19 +1,31 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 import { BioEvoService } from './bioevo.service';
 
 describe('BioEvoService', () => {
+
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let bioEvoService: BioEvoService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BioEvoService],
-	  imports: [
-	    HttpClientModule
-	  ]
+      imports: [ HttpClientTestingModule ],
+      providers: [ BioEvoService ]
     });
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    bioEvoService = TestBed.get(BioEvoService);
+  });
+  
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', inject([BioEvoService], (service: BioEvoService) => {
     expect(service).toBeTruthy();
   }));
+  
 });
--- bioevo-client/src/app/shared/report/report.service.spec.ts
+++ bioevo-client/src/app/shared/report/report.service.spec.ts
@@ -1,19 +1,31 @@
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed, inject } from '@angular/core/testing';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 import { ReportService } from './report.service';
 
 describe('ReportService', () => {
+
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let reportService: ReportService;
+
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ReportService],
-	  imports: [
-	    HttpClientModule
-	  ]
+      imports: [ HttpClientTestingModule ],
+      providers: [ ReportService ]
     });
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    reportService = TestBed.get(ReportService);
+  });
+  
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', inject([ReportService], (service: ReportService) => {
     expect(service).toBeTruthy();
   }));
+  
 });
