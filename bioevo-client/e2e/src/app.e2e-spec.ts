e6a36e76229a11333f6e8d8b1d458a72e364d94b
Marko Urm <markourm@gmail.com>
2018-08-29 13:06:32 +0100

    added protractor tests; downgraded nginx alpine to stable version due to issues with the latest 1.15

--- bioevo-client/e2e/src/app.e2e-spec.ts
+++ bioevo-client/e2e/src/app.e2e-spec.ts
@@ -1,14 +1,63 @@
-import { AppPage } from './app.po';
+import { by } from 'protractor';
 
-describe('workspace-project App', () => {
-  let page: AppPage;
+import {
+  LandingPage,
+  NotFoundPage,
+  WorldListPage,
+  WorldDetailsPage
+ } from './app.po';
+
+describe('BioEvo user journeys', () => {
+  let home: LandingPage;
+  let notFound: NotFoundPage;
+  let worldList: WorldListPage;
+  let details: WorldDetailsPage;
 
   beforeEach(() => {
-    page = new AppPage();
+    home = new LandingPage();
+    notFound = new NotFoundPage();
+    worldList = new WorldListPage();
+    details = new WorldDetailsPage();
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to BioEvo!');
+  it('should display welcome message in landing page', () => {
+    home.navigateTo();
+    expect(home.isDisplayed()).toBe(true);
+    expect(home.getText()).toEqual('Welcome to BioEvo!');
   });
+
+  it('should display Not Found Page given unknown path', () => {
+    notFound.navigateTo();
+    expect(notFound.isDisplayed()).toBe(true);
+    expect(notFound.getText()).toEqual('This page doesn\'t exist. Go back to home.');
+
+    notFound.getHomeLink().click();
+    expect(home.isDisplayed()).toBe(true);
+  });
+
+  it('should navigate to World List', () => {
+    home.navigateTo();
+    home.getLoginButton().click();
+    expect(worldList.isDisplayed()).toBe(true);
+  });
+
+  it('should navigate to World Details', () => {
+    home.navigateTo();
+    home.getLoginButton().click();
+    worldList.getCreateWorldButton().click();
+
+    // const rows = worldList.getWorldListRows();
+    // expect(rows.count()).toBeGreaterThan(0);
+
+    // let lastRow = rows.last();
+    // const worldId = lastRow.findElement(by.css('.world-id')).getText();
+
+    // lastRow.findElement(by.css('.view-world-button')).click();
+    // expect(details.isDisplayed()).toBe(true);
+    // expect(details.getHeaderTitle()).toEqual('World ' + worldId);
+
+    // details.getBackToListButton().click();
+    // expect(worldList.isDisplayed()).toBe(true);
+  });
+
 });
--- bioevo-client/e2e/src/app.po.ts
+++ bioevo-client/e2e/src/app.po.ts
@@ -1,11 +1,93 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, $ } from 'protractor';
 
-export class AppPage {
+export class LandingPage {
+
+  isDisplayed() {
+    return $('.bioevo-landing-page').isPresent();
+  }
+
   navigateTo() {
     return browser.get('/');
   }
 
-  getParagraphText() {
-    return element(by.css('.bioevo-home-text')).getText();
+  getText() {
+    return $('.bioevo-home-text').getText();
+  }
+
+  getLoginButton() {
+    return $('.login-button');
+  }
+}
+
+export class NotFoundPage {
+
+  isDisplayed() {
+    return $('.not-found-page').isPresent();
+  }
+
+  navigateTo() {
+    return browser.get('/notExisting');
+  }
+
+  getText() {
+    return $('p').getText();
+  }
+
+  getHomeLink() {
+    return $('p a');
+  }
+}
+
+export class WorldListPage {
+
+  isDisplayed() {
+    return $('.world-list-title').isPresent();
+  }
+
+  navigateTo() {
+    return browser.get('/bioevo-front');
+  }
+
+  getTitle() {
+    return $('.world-list-title').getText();
+  }
+
+  getWorldListHeader() {
+    return $('.world-list-header');
+  }
+
+  getWorldListRows() {
+    return $('.world-list-table').all(by.css('.world-list-row'));
+  }
+
+  getCreateWorldButton() {
+    return $('.create-world-button');
+  }
+}
+
+export class WorldDetailsPage {
+
+  isDisplayed() {
+    return $('.world-details').isPresent();
+  }
+
+  navigateTo(worldId: number) {
+    return browser.get('/bioevo-front/' + worldId);
+  }
+
+  getHeaderTitle() {
+    return $('.world-details-header-title').getText();
+  }
+
+  getText() {
+    return $('.world-details-content').getText();
+  }
+
+  getAdvanceStepButton() {
+    return $('.world-details-step-button');
+  }
+
+  getBackToListButton() {
+    return $('.world-details-world-list-button');
   }
 }
